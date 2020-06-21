4dbe25f6d7fb64d9ea486b3dd823264de51ba4b6
ozelotjp <contact@ozelot.app>
2020-06-21 14:58:28 +0900

    chore: firestoreを有効化

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,6 +1,7 @@
 import { Plugin } from '@nuxt/types'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 import 'firebase/functions'
 
 const firebaseConfig = {
